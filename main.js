36a41fd8d487c9e9089b3614c4f3ceb6e7ee4e9a
Ann-Fox <fx8565@mail.ru>
2025-05-08 16:02:21 +0500

    feat: add label input - not finnish

--- main.js
+++ main.js
@@ -1,10 +1,43 @@
 let divElement = document.querySelector("div");
 let inputElement = document.querySelector("input");
 
+let labesElement = document.querySelector(".labes");
+let inputLabesElement = document.querySelector(".input-label");
+
 document.addEventListener("DOMContentLoaded", function () {
     inputElement.oninput = function () {
         // Отслеживаем ввод в поле input
         divElement.innerText = this.value; // В div выводим вводимый текст
-        console.log(this.value);
+        // console.log(this.value);
     };
+
+    // inputLabesElement.oninput = function () {
+    //     labesElement.innerText = this.value
+    // }
+});
+
+inputLabesElement.addEventListener("keyup", function (event) {
+    let array = [];
+    // console.log(event.target.value);
+    let arrayInput = event.target.value.split(",");
+
+    for (let index = 0; index < arrayInput.length; index++) {
+        const element = arrayInput[index].trim();
+        // console.log(element);
+        if (element != "") {
+            array.push(element);
+        }
+    }
+    const messageElement = document.createElement("div");
+
+    if (event.code == "Comma") {
+        for (let index = 0; index < array.length; index++) {
+            const element = array[index];
+            console.log(element);
+            messageElement.classList.add("message");
+            messageElement.innerText = element;
+            labesElement.appendChild(messageElement);
+        }
+    }
+    console.log(array);
 });
